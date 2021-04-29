cbf50a3048461ea003c8ad1a54ce14248c9f5886
jinahgna <jinahgna@gmail.com>
2021-04-29 11:48:59 +0900

    commit test3

--- src/views/userInfoWrite.vue
+++ src/views/userInfoWrite.vue
@@ -140,7 +140,7 @@ export default {
 			this.userCorp = this.userDetail.curr_corp;
 		},
 		async actionIdCheck() {
-			console.log('test commit2');
+			console.log('test commit');
 			const idCheckData = {
 				user_id: this.userId,
 			};
